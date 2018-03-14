9576e8b18b0c3329357945185dc5fe1bee30261a
Michelle Olivieri <michelleo72@gmail.com>
2018-03-14 13:25:15 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -15,11 +15,3 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def move(board, index, character = "X")
   board[index] = character
 end
-
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
